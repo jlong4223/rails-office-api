0f57045e00b998f54662259b061f0385d56823cc
Jared Long <jlong4223@gmail.com>
2021-03-25 12:26:31 -0500

    sets up migration and schema for the user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_011501) do
+ActiveRecord::Schema.define(version: 2021_03_25_162512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2021_03_25_011501) do
   create_table "offices", force: :cascade do |t|
     t.string "author"
     t.string "quote"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
