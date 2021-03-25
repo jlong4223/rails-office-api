26a7c76b2b7b5e74897ab661e3bcc4e5df822db3
Jared Long <jlong4223@gmail.com>
2021-03-24 20:28:02 -0500

    adds route to welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
     def index
-        render json: {status: 200, message: "Welcome to the Office Quotes API", created_by: "Jared Long"}
+        render json: {status: 200, message: "Welcome to the Office Quotes API", created_by: "Jared Long", routes: "/offices"}
     end 
 end
