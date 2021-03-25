35658b517d7e452a7f6a35132cae8283f6e9c594
Jared Long <jlong4223@gmail.com>
2021-03-25 12:24:52 -0500

    sets presence to true and email to having uniqueness

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    validates :name, presence: true 
+    # if an email is inputted with uppercase or lowercase letters, it will not allow for matches 
+    validates :email, presence: true, :uniqueness => {:case_sensitive => false} 
+    validates :password, presence: true
+end
