99d1e46934ed5a6153e26b04eee688d154e414c2
Jared Long <jlong4223@gmail.com>
2021-03-26 17:40:47 -0500

    adds custom methods for getting login function ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,21 +7,39 @@ class UsersController < ApplicationController
         render json: @users
     end
 
+    # = testing custom route with getting users here as well
+    def login
+        @users = User.all
+        render json: User.all
+    end
+
     # GET user/id
     def show
         render json: @user
     end
 
+    # POST's a new user
     def create
         @user = User.new(user_params)
-
+        
         if @user.save
             render json: @user, status: :created, location: @user
         else
             render json: @user.errors, status: :unprocessable_entity
         end
     end
+    
+    # TODO create a create route for this method and install jwt gem:
+    def authenticate_user
+        user = User.find_for_database_authentication(email: params[:email])
+        if user.valid_password?(params[:password])
+            render json: payload(user)
+        else
+            render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
+        end
+    end
 
+    # PUT
     def update
         if @user.update(user_params)
             render json: @user
@@ -30,13 +48,12 @@ class UsersController < ApplicationController
           end
     end
 
+    # DELETE
     def destroy
         @user.destroy
     end
 
     private
-
-    # TODO hash password here? and add to the before_action?
 
     def set_user
         @user = User.find(params[:id])
