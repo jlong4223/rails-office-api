2388c6066e97fcfd1f3c6f79c1c6fb9cc04cbb36
Jared Long <jlong4223@gmail.com>
2021-03-26 17:41:06 -0500

    creates custom routes for prepping login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  
+  # setting the users/login about the users resource so it doesnt mess with the id param
+  get "/users/login", to: 'users#login'
+
+  # post route for signing jwt
+  post "/users/login", to: 'users#authenticate_user'
+
   resources :users
   resources :offices
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
   resources :notices
 end
