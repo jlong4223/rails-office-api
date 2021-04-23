b65ee48c810487726062ebbc0c7db6e43e3b49d1
Jared Long <jlong4223@gmail.com>
2021-04-22 22:11:39 -0500

    adds relationship between users and favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
     validates :email, presence: true, :uniqueness => {:case_sensitive => false} 
     validates :password, presence: true
 
+    has_many :favorites
 end
